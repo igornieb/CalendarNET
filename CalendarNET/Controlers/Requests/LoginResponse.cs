2771521e42404f0bf18bf88962c99ffdf6279a60
igornieb <igornieb@gmail.com>
2023-04-25 21:17:31 +0200

    added User classes that represents request/response

---
+++ CalendarNET/Controlers/Requests/LoginResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace CalendarNET.Controlers.Requests
+{
+    public class LoginResponse
+    {
+        public string Username { get; set; }
+        public string Token { get; set; }
+    }
+}
---
+++ CalendarNET/Controlers/Requests/UserRequest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Build.Framework;
+
+namespace CalendarNET.Controlers.Requests
+{
+    public class UserRequest
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }        
+        public string? NewPassword { get; set; } = null;
+        public string? CurrentPassword { get; set; } = null;
+
+    }
+}

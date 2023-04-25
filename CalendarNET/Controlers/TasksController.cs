8a78e04f0dbeedaf58e462cf502584c331a3cc17
igornieb <igornieb@gmail.com>
2023-04-25 22:15:38 +0200

    added user authentication

--- CalendarNET/Controlers/TasksController.cs
+++ CalendarNET/Controlers/TasksController.cs
@@ -4,6 +4,7 @@ using CalendarNET.Data;
 using CalendarNET.Controlers.Requests;
 using Microsoft.AspNetCore.Identity;
 using CalendarNET.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace CalendarNET.Controlers
 {
@@ -21,8 +22,8 @@ namespace CalendarNET.Controlers
         }
 
         // GET: api/Tasks
-        // returns today tasks
-        [HttpGet]
+        // return today tasks
+        [HttpGet, Authorize]
         [Route("today")]
         public async Task<ActionResult<IEnumerable<Models.Task>>> GetTodayTaskCollection()
         {
@@ -30,12 +31,13 @@ namespace CalendarNET.Controlers
             {
                 return NotFound();
             }
-            var today_tasks = await _context.TaskCollection.Where(task => task.DueOn.Date == DateTime.UtcNow.Date).ToListAsync();
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            var today_tasks = await _context.TaskCollection.Where(task => task.DueOn.Date == DateTime.UtcNow.Date && task.UserId == user.Id).ToListAsync();
             return today_tasks;
 
         }
 
-        [HttpGet("{year}/{month}/{day}")]
+        [HttpGet("{year}/{month}/{day}"), Authorize]
         // GET: api/Task/2022/12/21
         public async Task<ActionResult<IEnumerable<Models.Task>>> GetDateTaskCollection(int year, int month, int day)
         {
@@ -44,19 +46,21 @@ namespace CalendarNET.Controlers
                 return NotFound();
             }
             DateTime date = DateTime.Parse($"{day}/{month}/{year}");
-            var today_tasks = await _context.TaskCollection.Where(task => task.DueOn.Date == date.Date).ToListAsync();
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            var today_tasks = await _context.TaskCollection.Where(task => task.DueOn.Date == date.Date && task.UserId==user.Id).ToListAsync();
             return today_tasks;
         }
 
         // GET: api/Tasks/{id}
-        [HttpGet("{id}")]
+        [HttpGet("{id}"), Authorize]
         public async Task<ActionResult<Models.Task>> GetTaskId(int id)
         {
           if (_context.TaskCollection == null)
           {
               return NotFound();
           }
-            var task = _context.TaskCollection.FirstOrDefault(task => task.Id==id);
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            var task = _context.TaskCollection.FirstOrDefault(task => task.Id==id && task.UserId==user.Id);
 
             if (task == null)
             {
@@ -68,10 +72,11 @@ namespace CalendarNET.Controlers
 
         // PUT: api/Tasks/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
+        [HttpPut("{id}"), Authorize]
         public async Task<IActionResult> EditTask(int id, TaskRequest task)
         {
-            var current_task = await _context.TaskCollection.FindAsync(id);
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            var current_task = await _context.TaskCollection.FirstOrDefaultAsync(task => task.Id == id && task.UserId == user.Id);
             if (current_task == null)
             {
                 return NotFound();
@@ -90,29 +95,30 @@ namespace CalendarNET.Controlers
 
         // POST: api/Tasks
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Models.Task>> PostTask(TaskRequest task)
+        [HttpPost, Authorize]
+        public async Task<ActionResult<TaskRequest>> PostTask(TaskRequest task)
         {
           if (_context.TaskCollection == null)
           {
               return Problem("Empty set");
           }
-
-            var taskDb = new Models.Task { Name = task.Name, Description = task.Description, DueOn = task.DueOn, shared = task.shared };
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            var taskDb = new Models.Task { Name = task.Name, Description = task.Description, DueOn = task.DueOn, shared = task.shared, UserId=user.Id, User=user };
             _context.TaskCollection.Add(taskDb);
             await _context.SaveChangesAsync();
             return CreatedAtAction("GetTaskId", new { id = taskDb.Id }, taskDb);
         }
 
         // DELETE: api/Tasks/5
-        [HttpDelete("{id}")]
+        [HttpDelete("{id}"), Authorize]
         public async Task<IActionResult> DeleteTask(int id)
         {
             if (_context.TaskCollection == null)
             {
                 return NotFound();
             }
-            var task = await _context.TaskCollection.FindAsync(id);
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            var task = await _context.TaskCollection.FirstOrDefaultAsync(task => task.Id == id && task.UserId == user.Id);
             if (task == null)
             {
                 return NotFound();

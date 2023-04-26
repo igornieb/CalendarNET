e185348e0cf9fbdf158a64fb463379296eb1ab72
igornieb <igornieb@gmail.com>
2023-04-26 14:55:35 +0200

    folder name change

---
+++ CalendarNET/Controlers/Serializers/UserResponse.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Build.Framework;
+
+namespace CalendarNET.Controlers.Requests
+{
+    public class UserResponse
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+    }
+}
